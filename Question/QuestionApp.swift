4d6ef2d9e0ac170899696aab2ca141b4718afbde
hira22 <masayuki.hiraoka@ietty.co.jp>
2020-11-16 11:23:16 +0900

    Initial Commit

---
+++ Question/QuestionApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionApp.swift
+//  Question
+//
+//  Created by hiraoka on 2020/11/16.
+//
+
+import SwiftUI
+
+@main
+struct QuestionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
